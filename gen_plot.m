c34cf75c25700541fdd886bd6897dba8b8e24165
Alessio Ragno <spideralessio97@gmail.com>
2020-07-18 18:25:42 +0200

    fix imgs

--- gen_plot.m
+++ gen_plot.m
@@ -3,10 +3,9 @@ clc
 close all
 plots = {'MTN', 'MBP', 'MTIWN', 'MBIWP', 'MTSIWN', 'MBSIWP'}
 
-figure
-tiledlayout(1,2)
+gcf = figure
 
-nexttile
+subplot(1,2,1)
 hold on
 dt = 0.001;
 T = 1.8221;
@@ -18,9 +17,9 @@ end
 
 xlabel('time')
 ylabel('joint velocity norm')
-legend(plots, 'Location', 'northwest')
+% legend(plots, 'Location', 'northwest')
 
-nexttile
+subplot(1,2,2)
 hold on
 for i=1:length(plots)
     load(join([plots{i}, '/data.mat']))
@@ -28,6 +27,55 @@ for i=1:length(plots)
 end
 xlabel('time')
 ylabel('torque norm')
+le = legend(plots, 'Orientation', 'horizontal')
+set(le, 'Position', [0.4 0 0.2 0.05]);
+set(gcf,'position',[100 100 1800 800])
+print(gcf, 'imgs/vel_torque_plots.png','-dpng', '-r150')
+gcf = figure
 legend(plots, 'Location', 'northwest')
+for j=1:3 
+    %disp(j)
+    x = (j-1)*3 + 1
+    subplot(3, 3, x)
+    
+    hold on
+    dt = 0.001;
+    T = 1.8221;
+    t = 0:dt:T;
+    for i=1:length(plots)
+        load(join([plots{i}, '/data.mat']))
+        plot(t, qs(j,:))
+    end
 
+    xlabel('time')
+    ylabel(strcat('joint',  int2str(j), ' value'))
+    
 
+    subplot(3, 3, x+1)
+    hold on
+    dt = 0.001;
+    T = 1.8221;
+    t = 0:dt:T;
+    for i=1:length(plots)
+        load(join([plots{i}, '/data.mat']))
+        plot(t, qds(j,:))
+    end
+
+    xlabel('time')
+    ylabel(strcat('joint', int2str(j), ' velocity'))
+    %legend(plots, 'Location', 'northwest')
+
+    subplot(3, 3, x+2)
+    hold on
+    for i=1:length(plots)
+        load(join([plots{i}, '/data.mat']))
+        plot(t, ts(j,:))
+    end
+    xlabel('time')
+    ylabel(strcat('joint', int2str(j), ' torque'))
+    %legend(plots, 'Location', 'northwest')
+end
+le = legend(plots, 'Orientation', 'horizontal')
+set(le, 'Position', [0.4 0 0.2 0.05]);
+set(gcf,'position',[100 100 1800 800])
+print(gcf, 'imgs/joints_plots.png','-dpng', '-r150')
